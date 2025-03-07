1cf0f24c4e7b26de6acff1ae55c7ab4d33150ce9
Zhenting Ling <zhenting.ling@stonybrook.edu>
2025-03-07 10:57:07 -0500

    setup-backend.sh now deletes the existing virtual environment.

--- backend/setup-backend.sh
+++ backend/setup-backend.sh
@@ -1,3 +1,7 @@
+# Remove existing virtual environment.
+rm -rf musk-venv
+
+# Install Python 3, pip, and venv if not already installed.
 sudo apt update
 sudo apt install python3
 sudo apt install python3-pip
@@ -5,7 +9,7 @@ sudo apt install python3-venv
 
 source ~/.bashrc
 
+# Create a new virtual environment and install dependencies.
 python3 -m venv musk-venv
-
 source musk-venv/bin/activate
 pip install -r requirements.txt
