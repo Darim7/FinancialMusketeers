219237e093b8780f7d3494dd1025aacb24a492dc
Maggie <maggie.lin@stonybrook.edu>
2025-03-15 18:48:19 -0400

    Added Google OAuth

---
+++ frontend/src/pages/Overview.tsx
@@ -0,0 +1,15 @@
+// MainPage.js
+import React from 'react';
+import './Overview.css';
+import NavBar from '../components/NavBar';
+
+function Overview() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default Overview;  // This ensures MainPage can be imported elsewhere
+
