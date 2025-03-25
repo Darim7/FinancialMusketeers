274e6f7c7e8471163a442d47c9925039ec75b8dd
Kelly Chen <kellychen966@gmail.com>
2025-03-24 20:44:05 -0400

    adding additional test case for frontend

--- frontend/playwright/tests/components/scenarios.spec.ts
+++ frontend/playwright/tests/components/scenarios.spec.ts
@@ -14,6 +14,18 @@ test.describe('scenarios page', () => {
         await expect(content).toContain("Export")
     })
 
+    test('has import on guest scenarios', async ({page})=> {
+        await page.goto(path);
+        click_on_guest(page);
+        click_scenarios(page);
+        const exportElmt = page.locator('button.btn.btn-primary:has-text("+ Import")');
+        await expect(exportElmt).toBeVisible()
+        await expect(exportElmt).toBeEnabled()
+
+        const content=await exportElmt.textContent()
+        await expect(content).toContain("Import")
+    })
+
     test('has create new on guest scenarios', async ({page})=> {
         await page.goto(path);
         click_on_guest(page);
