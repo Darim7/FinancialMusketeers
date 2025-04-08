92d24375c5b72e8e6211f00edd9276caffab611f
Zhenting Ling <zhenting.ling@stonybrook.edu>
2025-04-08 16:51:47 -0400

    Sketch update_inflation().

--- backend/muskets/simulations.py
+++ backend/muskets/simulations.py
@@ -1,10 +1,33 @@
 import numpy as np
+from models.scenario import Scenario
+from models.tax import FederalTax, StateTax
 
-def update_inflation(tax_obj, inflation_assumption):
+def update_inflation(tax_obj, inflation_assumption: dict) -> float:
     """
     Update the inflation rate and all of the inflation-related values
     """
-    pass
+    inflation_rate = -1
+
+    # Check to see if the inflation assumption is a distribution or a single value
+    if inflation_assumption['type'] == 'fixed':
+        inflation_rate = inflation_assumption['value']
+    elif inflation_assumption['uniform']:
+        inflation_rate = np.random.uniform(
+            inflation_assumption['lower'],
+            inflation_assumption['upper'],)
+    else:
+        inflation_rate = np.random.normal(
+            inflation_assumption['mean'],
+            inflation_assumption['stdev'],)
+
+    # Update the tax bracket
+    res = {}
+    for bracket, percentage in tax_obj.bracket.items():
+        new_bracket = bracket * (1 + inflation_rate)
+        res[new_bracket] = percentage
+    tax_obj.bracket = res
+
+    return inflation_rate
 
 def update_investments():
     """
@@ -13,8 +36,8 @@ def update_investments():
     pass
 
 def perform_rmd(rmd_obj, age: int)-> float:
-    """ Performs the required minimum distribution (RMD) for previous year
-
+    """
+    Performs the required minimum distribution (RMD) for previous year
 
     Args:
         rmd_obj (RMD): RMD object
@@ -23,7 +46,7 @@ def perform_rmd(rmd_obj, age: int)-> float:
     Returns:
         float: RMD amount
     """
-    pass
+    return -1
 
 def fed_income_tax(tax_obj, income: float) -> float:
     """
@@ -36,23 +59,7 @@ def fed_income_tax(tax_obj, income: float) -> float:
     Returns:
         float: The calculated federal income tax.
     """
-    # Load tax brackets from YAML file
-    with open(os.path.join(os.path.dirname(__file__), 'tax_brackets.yaml'), 'r') as file:
-        tax_brackets = yaml.safe_load(file)
-
-    # Get the appropriate tax brackets for the given filing status
-    brackets = tax_brackets[status]
-
-    # Calculate the federal income tax
-    tax = 0.0
-    for bracket in brackets:
-        if income > bracket['income']:
-            tax += (bracket['income'] - bracket['previous_income']) * bracket['rate']
-        else:
-            tax += (income - bracket['previous_income']) * bracket['rate']
-            break
-
-    return tax
+    return -1
 
 def state_income_tax(income: int, status: str) -> float:
     """
@@ -63,23 +70,7 @@ def state_income_tax(income: int, status: str) -> float:
     Returns:
         float: The calculated state income tax.
     """
-    # Load tax brackets from YAML file
-    with open(os.path.join(os.path.dirname(__file__), 'state_tax_brackets.yaml'), 'r') as file:
-        tax_brackets = yaml.safe_load(file)
-
-    # Get the appropriate tax brackets for the given filing status
-    brackets = tax_brackets[status]
-
-    # Calculate the state income tax
-    tax = 0.0
-    for bracket in brackets:
-        if income > bracket['income']:
-            tax += (bracket['income'] - bracket['previous_income']) * bracket['rate']
-        else:
-            tax += (income - bracket['previous_income']) * bracket['rate']
-            break
-
-    return tax
+    return -1
 
 def income_calculation(tax_obj, inflation_assumption, income: float) -> float:
     return -1
