0e58542790813233e35faf50a738e33116113373
Zhenting Ling <zhenting.ling@stonybrook.edu>
2025-05-05 21:52:14 -0400

    Add income_breakdown, expense_breakdown, and investment_breakdown in run_year() result.

--- backend/muskets/simulations.py
+++ backend/muskets/simulations.py
@@ -437,7 +437,7 @@ def discretionary_expenses(event_series: list[EventSeries], spending_strategy: l
         if event.type == 'expense' and 'discretionary' in event.data and event.data['discretionary'] and check_event_start(event, year):
             discretionary_expenses[event.name] = event.data['initialAmount']
 
-    return [discretionary_expenses[spending] for spending in spending_strategy if spending in discretionary_expenses]
+    return [(discretionary_expenses[spending], spending) for spending in spending_strategy if spending in discretionary_expenses]
 
 def make_investments(invest_event: EventSeries, investments: list[Investment], year: int) -> float:
     """
@@ -571,6 +571,7 @@ def run_year(scenario: Scenario, year: int, state_tax: StateTax, fed_tax: Federa
 
     # STEP 7: Calculate non-discretionary
     non_discresionary_expenses_value = non_discretionary_expenses(event_series, year)
+    expenses_breakdown = {event.name: event.data['initialAmount'] for event in event_series if event.data['type'] == 'expense' and not event.data['discretionary'] and check_event_start(event, year)}
     discretionary_expenses_value = discretionary_expenses(event_series, scenario.spending_strat, year)
 
     logger.info(f"Non-discretionary expenses: {non_discresionary_expenses_value}, Discretionary expenses: {discretionary_expenses_value}")
@@ -594,11 +595,14 @@ def run_year(scenario: Scenario, year: int, state_tax: StateTax, fed_tax: Federa
     # Pay discretionary expenses
     q = deque(discretionary_expenses_value)
     while cash_investment.value > 0 and q:
-        expense = round(q.popleft(), 2)
+        expense_amount, expense_name = q.popleft()
+        expense = round(expense_amount, 2)
         if cash_investment.value >= expense:
             cash_investment.value -= expense
+            expenses_breakdown[expense_name] = expense
         else:
             # If the current year income is less than the expense, pay partial.
+            expenses_breakdown[expense_name] = cash_investment.value
             cash_investment.value = 0
             break
 
@@ -632,6 +636,19 @@ def run_year(scenario: Scenario, year: int, state_tax: StateTax, fed_tax: Federa
 
     logger.info(f"Current year net worth: {currYearSum}, Financial goal: {scenario.financial_goal}")
 
+    investment_breakdown = {invest.investment_id: invest.value for invest in investments}
+
+    income_breakdown = {
+        'gross_income': gross_income_value,
+        'social_security': social_security_income,
+        'capital_gains': capital_gains,
+        'current_year_income': currYearIncome,
+    }
+
+    logger.info(f'income breakdown: {income_breakdown}')
+    logger.info(f'expenses breakdown: {expenses_breakdown}')
+    logger.info(f"Investment values: {investment_breakdown}")
+
     return {
         'federal_tax': federal_tax_value,
         'state_tax': state_tax_value,
@@ -641,7 +658,9 @@ def run_year(scenario: Scenario, year: int, state_tax: StateTax, fed_tax: Federa
         'amount_invested': amount_invested,
         'amount_rebalanced': amount_rebalanced,
         'financial_goal': currYearSum >= scenario.financial_goal,
-        'investment_values': {invest.investment_id: invest.value for invest in investments},
+        'investment_values': investment_breakdown,
+        'income_breakdown': income_breakdown,
+        'expenses_breakdown': expenses_breakdown,
     }
 
 def save_logs_to_csv(logs: list[dict], filename: str) -> None:
@@ -713,7 +732,6 @@ def run_simulation(scenario: Scenario, user: str, num_sim: int) -> list[dict]:
         
         year_investments = {'year': year}
         for invest in scenario.get_investments():
-            logger.info(f"Investment ID: {invest.investment_id}, Value: {invest.value}")
             year_investments[invest.investment_id] = round(invest.value, 2)
         investment_logs.append(year_investments)
         
